39e75c71ac015f75fdd28c44b624153272d0df11
atirake <54449819+atirake@users.noreply.github.com>
2022-09-07 21:23:13 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
 class ArrayOperations {
     public static void reverseElements(int[][] twoDimArray) {
-        for (int i = 0; i < twoDimArray.length; i++) {
-            int[] tempArray = new int[twoDimArray[0].length];
+        for (int[] rowArray : twoDimArray) {
+            int[] tempArray = new int[rowArray.length];
             int index = 0;
-            for (int j = twoDimArray[0].length - 1; j >= 0; j--) {
-                tempArray[index] = twoDimArray[i][j];
+            for (int j = rowArray.length - 1; j >= 0; j--) {
+                tempArray[index] = rowArray[j];
                 index += 1;
             }
-            index = 0;
-            for (int j = 0; j < twoDimArray[0].length; j++) {
-                twoDimArray[i][j] = tempArray[index];
-                index += 1;
-            }
+            System.arraycopy(tempArray, 0, rowArray, 0, rowArray.length);
         }
     }
 }
